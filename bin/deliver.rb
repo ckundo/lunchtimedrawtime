e19b4bb0456fbd7ef1a950a24064db5a97af6e23
Cameron Cundiff <pair+ccundiff@pivotallabs.com>
2014-10-02 18:29:01 -0400

    A basic Twilio app.

---
+++ bin/deliver.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+@client.account.messages.create({
+  :from => ENV['TWILIO_FROM'],
+  :to => ENV['TWILIO_TO'],
+  :body => 'Draw a horse',
+})
