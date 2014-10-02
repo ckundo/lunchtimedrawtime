1c8a52c33125af428699c98889a50e20fb98eb02
Cameron Cundiff <pair+ccundiff@pivotallabs.com>
2014-10-02 19:25:52 -0400

    Read ideas list from a flat file.

--- bin/deliver.rb
+++ bin/deliver.rb
@@ -2,14 +2,14 @@
 require 'rubygems'
 require 'twilio-ruby'
 
-# put your own credentials here
+ideas = File.expand_path('../../ideas.txt', __FILE__)
+message = "Drawtime! Draw #{File.readlines(ideas).sample}."
+
 account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 
-# set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new account_sid, auth_token
 
-message = "Draw #{['a horse', 'an elephant', 'your dream house', 'your spirit animal'].sample }"
 
 @client.account.messages.create({
   :from => ENV['TWILIO_FROM'],
